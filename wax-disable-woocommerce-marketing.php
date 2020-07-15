e9e5d0561760336043b7cacb79fbf4ec7c983892
Webaxones <contact@webaxones.com>
2020-07-15 15:54:54 +0200

    Initial commit

---
+++ wax-disable-woocommerce-marketing.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Plugin Name: WAX disable woocommerce marketing
+ * Plugin URI: https://www.webaxones.com
+ * Description: Disable WooCommerce Marketing
+ * Author: Webaxones
+ * Author URI: https://www.webaxones.com
+ * Version: 1.0
+ */
+
+// don't load directly.
+if ( ! defined( 'ABSPATH' ) ) :
+	die( '-1' );
+endif;
+
+/**
+ * Disable WooCommerce Marketing
+ */
+
+add_filter( 'woocommerce_marketing_menu_items', '__return_empty_array' );
+add_filter( 'woocommerce_admin_features', 'wax_disable_woocommerce_admin_features' );
+
+function wax_disable_woocommerce_admin_features( $features ) {
+	$marketing = array_search( 'marketing', $features, true );
+	unset( $features[ $marketing ] );
+	return $features;
+}
